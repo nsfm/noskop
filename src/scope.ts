d81eb177d4970d753ae282dfa410fb5aa6f64825
Nate Dube <nate@ndube.com>
2022-05-19 20:10:59 -0700

    Unify commands

--- src/scope.ts
+++ src/scope.ts
@@ -6,6 +6,9 @@ interface Command {
   reject: (reason: string) => void;
   command: string;
   requested: number;
+  priority: boolean;
+  response?: string;
+  success?: boolean;
   send?: number;
   sent?: number;
   completed?: number;
@@ -41,16 +44,21 @@ export class Scope {
     this.setStepsPerUnit("Z", 30);
   }
 
-  cmd(command: string, priority: boolean = false): Promise<Command> {
+  command(command: string, priority: boolean = false): Promise<Command> {
     return new Promise((resolve, reject) => {
       const cmd: Command = {
         command,
         resolve,
         reject,
         requested: Date.now(),
+        priority,
       };
       priority ? this.commandQueue.unshift(cmd) : this.commandQueue.push(cmd);
     });
+  }
+
+  priorityCommand(command: string): Promise<Command> {
+    return this.command(command, true);
   }
 
   transmit(): void {
@@ -62,6 +70,17 @@ export class Scope {
       this.port.write(this.pendingCommand.command);
       this.pendingCommand.sent = Date.now();
     }
+  }
+
+  // Call this when something unexpected happens
+  unexpected(reason: string): void {
+    console.error(`unexpected: ${reason}`);
+    this.stop()
+      .then(({ success }) => {
+        if (!success) process.exit(9);
+        console.error("unexpected: stopped");
+      })
+      .catch(console.error);
   }
 
   // Handle feedback from the device
@@ -79,171 +98,164 @@ export class Scope {
     y: number;
     z: number;
     e: number;
-  }): this {
-    this.port.write(`G0 X${x} Y${y} Z${z} E${e} F${this.feedrate}\n`);
-    return this;
+  }): Promise<Command> {
+    return this.command(`G0 X${x} Y${y} Z${z} E${e} F${this.feedrate}`);
   }
 
   // TODO
-  arcMove(): this {
-    this.port.write(`G2 I20 J20`);
-    return this;
+  arcMove(): Promise<Command> {
+    return this.command(`G2 I20 J20`);
   }
 
-  home(): this {
-    const stageLower = 5; // mm
-    this.port.write(`G28\n O R${stageLower} X Y Z`);
-    return this;
+  // TODO mm to lower
+  home(stageLower: number = 5): Promise<Command> {
+    return this.command(`G28\n O R${stageLower} X Y Z`);
   }
 
-  absoluteMode(): this {
-    this.port.write("G90\n");
-    return this;
+  absoluteMode(): Promise<Command> {
+    return this.command("G90");
   }
 
-  relativeMode(): this {
-    this.port.write("G91\n");
-    return this;
+  relativeMode(): Promise<Command> {
+    return this.command("G91");
   }
 
-  steppersOn(): this {
-    this.port.write("M17\n");
-    this.steppersEnabled = true;
-    return this;
+  steppersOn(): Promise<Command> {
+    const result = this.command("M17");
+    result
+      .then(({ success }) => {
+        success ? (this.steppersEnabled = true) : this.unexpected("A");
+      })
+      .catch(() => {
+        this.unexpected("B");
+      });
+    return result;
   }
 
-  // speed: 0 -255
-  setFan(index: number, speed: number): this {
-    this.port.write(`M106 I${index} S${speed}\n`);
-    return this;
+  setFan(index: number, speed: number): Promise<Command> {
+    if (speed < 0 || speed > 255) throw new Error("Fan speed outta bounds");
+    return this.command(`M106 I${index} S${speed}`);
   }
 
-  disableFan(index: number): this {
-    this.port.write(`M107 I${index}\n`);
-    return this;
+  disableFan(index: number): Promise<Command> {
+    return this.command(`M107 I${index}`);
   }
 
   // Inactivity - seconds until automatic shutoff without movement
-  steppersOff(inactivity?: number): this {
-    this.port.write(`M18\n ${inactivity ? `S${inactivity}` : ""}`);
-    this.steppersEnabled = false;
-    return this;
+  steppersOff(inactivity?: number): Promise<Command> {
+    const result = this.command(`M18 ${inactivity ? `S${inactivity}` : ""}`);
+    result
+      .then(({ success }) => {
+        success ? (this.steppersEnabled = false) : this.unexpected("C");
+      })
+      .catch(() => {
+        this.unexpected("D");
+      });
+    return result;
   }
 
   // To millimeters
-  setTravelUnit(): this {
-    this.port.write("G21\n");
-    return this;
+  setTravelUnit(): Promise<Command> {
+    return this.command("G21");
   }
 
-  keepalive(interval: number): this {
-    this.port.write(`M113 S${interval}\n`);
-    return this;
-  }
-
-  // Drops homing
-  cancelMove(): this {
-    this.port.write("G80\n");
-    return this;
+  keepalive(interval: number): Promise<Command> {
+    return this.command(`M113 S${interval}`);
   }
 
   // Drops homing
-  stop(): this {
-    this.port.write("M112\n");
-    return this;
+  cancelMove(): Promise<Command> {
+    return this.priorityCommand("G80");
   }
 
   // Drops homing
-  quickstop(): this {
-    this.port.write("M410\n");
-    return this;
+  stop(): Promise<Command> {
+    return this.priorityCommand("M112");
   }
 
-  deployProbe(): this {
-    this.port.write("M401\n");
-    return this;
+  // Drops homing
+  quickstop(): Promise<Command> {
+    return this.priorityCommand("M410");
   }
 
-  stowProbe(): this {
-    this.port.write("M402\n");
-    return this;
+  deployProbe(): Promise<Command> {
+    return this.command("M401");
+  }
+
+  stowProbe(): Promise<Command> {
+    return this.command("M402");
   }
 
   // 0 to disable
-  setTemperatureInterval(seconds: number) {
-    this.port.write(`M155 S${seconds}\n`);
-    return this;
+  setTemperatureInterval(seconds: number): Promise<Command> {
+    return this.command(`M155 S${seconds}`);
   }
 
   // 0 to disable
-  setPositionInterval(seconds: number) {
-    this.port.write(`M154 S${seconds}\n`);
-    return this;
+  setPositionInterval(seconds: number): Promise<Command> {
+    return this.command(`M154 S${seconds}`);
   }
 
-  setStepsPerUnit(axis: "X" | "Y" | "Z" | "E", steps: number): this {
-    this.port.write(`M92 ${axis}${steps}\n`);
-    return this;
+  setStepsPerUnit(
+    axis: "X" | "Y" | "Z" | "E",
+    steps: number
+  ): Promise<Command> {
+    return this.command(`M92 ${axis}${steps}`);
   }
 
   // units-per-second
-  setMaxFeedrate(axis: "X" | "Y" | "Z" | "E", feedrate: number): this {
-    this.port.write(`M203 ${axis}${feedrate}\n`);
-    return this;
+  setMaxFeedrate(
+    axis: "X" | "Y" | "Z" | "E",
+    feedrate: number
+  ): Promise<Command> {
+    return this.command(`M203 ${axis}${feedrate}`);
   }
 
   // Apply a global speed modifier. 0 - 0% to 1 - 100%
-  setFeedrate(percentage: number): this {
-    this.port.write(`M203 S${Math.round(percentage * 100)}\n`);
-    return this;
+  setFeedrate(percentage: number): Promise<Command> {
+    return this.command(`M203 S${Math.round(percentage * 100)}`);
   }
 
   // units-per-second
-  setMinFeedrate(feedrate: number): this {
-    this.port.write(`M205 T${feedrate} S${feedrate}\n`);
-    return this;
+  setMinFeedrate(feedrate: number): Promise<Command> {
+    return this.command(`M205 T${feedrate} S${feedrate}`);
   }
 
   // units-per-second-squared
-  setMaxAcceleration(axis: "X" | "Y" | "Z" | "E", acceleration: number): this {
-    this.port.write(`M201 ${axis}${acceleration}\n`);
-    return this;
+  setMaxAcceleration(
+    axis: "X" | "Y" | "Z" | "E",
+    acceleration: number
+  ): Promise<Command> {
+    return this.command(`M201 ${axis}${acceleration}`);
   }
 
   // units-per-second
-  setMaxJerk(axis: "X" | "Y" | "Z" | "E", jerk: number): this {
-    this.port.write(`M205 ${axis}${jerk}\n`);
-    return this;
+  setMaxJerk(axis: "X" | "Y" | "Z" | "E", jerk: number): Promise<Command> {
+    return this.command(`M205 ${axis}${jerk}`);
   }
 
-  babystep(axis: "X" | "Y" | "Z", distance: number): this {
-    this.port.write(`M290 ${axis}${distance}\n`);
-    return this;
+  babystep(axis: "X" | "Y" | "Z", distance: number): Promise<Command> {
+    return this.command(`M290 ${axis}${distance}`);
   }
 
-  tone(milliseconds: number, frequency: number): this {
-    this.port.write(`M300 P${milliseconds} S${frequency}\n`);
-    return this;
+  tone(milliseconds: number, frequency: number): Promise<Command> {
+    return this.command(`M300 P${milliseconds} S${frequency}`);
   }
 
-  getEndstopStates(): this {
-    this.port.write("M119\n");
-    return this;
+  getEndstopStates(): Promise<Command> {
+    return this.command("M119");
   }
 
-  enableEndStops(): this {
-    this.port.write("M120\n");
-    return this;
+  enableEndStops(): Promise<Command> {
+    return this.command("M120");
   }
 
-  disableEndStops(): this {
-    this.port.write("M121\n");
-    return this;
+  disableEndStops(): Promise<Command> {
+    return this.command("M121");
   }
 
-  dwell(milliseconds: number): this {
-    this.port.write(`G4 P${milliseconds}`);
-    return this;
+  dwell(milliseconds: number): Promise<Command> {
+    return this.command(`G4 P${milliseconds}`);
   }
 
   setLED(
@@ -252,8 +264,9 @@ export class Scope {
     green: number,
     blue: number,
     brightness: number
-  ): this {
-    this.port.write(`G4 I${index} R${red} G${green} B${blue} P:${brightness} `);
-    return this;
+  ): Promise<Command> {
+    return this.command(
+      `G4 I${index} R${red} G${green} B${blue} P:${brightness}`
+    );
   }
 }
